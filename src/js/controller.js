ac8b65ebc023e751be0f2a75f519d1cf1d725a38
Tomson <twesolowski61@gmail.com>
2021-05-22 16:14:29 +0200

    updating results view to mark selected result

--- src/js/controller.js
+++ src/js/controller.js
@@ -17,7 +17,7 @@ const controlRecipes = async function () {
 
     if (!id) return;
     recipeView.renderSpinner();
-
+    resultsView.update(model.getSearchResultsPage());
     await model.loadRecipe(id);
 
     recipeView.render(model.state.recipe);
