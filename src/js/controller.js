6e5e625fe4392f2f9e8cd931152e3e4286b58019
Tomson <twesolowski61@gmail.com>
2021-05-29 12:03:19 +0200

    storing bookmarks in localstorage

--- src/js/controller.js
+++ src/js/controller.js
@@ -61,7 +61,12 @@ const controlAddBookmark = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
   recipeView.addHandlerBookmark(controlAddBookmark);
